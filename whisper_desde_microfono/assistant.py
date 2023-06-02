8d9f515d6f44890d716e0be0472422763ee118c0
Walter Bivanco <walterbivanco@gmail.com>
2023-06-02 12:59:45 -0300

    Video 4, Comienzo de lang chain, minuto 22:19

--- whisper_desde_microfono/assistant.py
+++ whisper_desde_microfono/assistant.py
@@ -1,19 +1,24 @@
+import dotenv
+import os
 import pywhatkit
 from  modules.listen import Listener
 from  modules.talk import Talk
 
 
+dotenv.load_dotenv()
+FAKEYOU_PASSWORD = os.getenv("FAKEYOU_PASSSWORD")
+
 def main():
     listener = Listener()
-    talker = Talk('usuario', 'clave', 'auronplay') # 'bob esponja'
+    talker = Talk('usuario', FAKEYOU_PASSWORD, 'auronplay') # 'bob esponja'
     try:
         response = listener.listen()
         if 'reproduce' in response:
             song = response.replace('reproduce', '')
             talker.talk(f'Reproduciendo {song}')
             pywhatkit.playonyt(song)    
-        else:
-            talker.talk(response) # Reproduce lo que uno dice
+        #else:
+            #talker.talk(response) # Reproduce lo que uno dice
     except Exception as e:
         talker.talk(f'Lo siento  no te entendí debido a este error: {e}')
         print(e)
---
+++ whisper_desde_microfono/templates/templates.py
@@ -0,0 +1,5 @@
+brain_template = (
+    'Eres un asistente que lo sabe absolutamente todo dispuesto a'
+    ' ayudar al usuario a responder a la pregunta que'
+    ' el usuario te haga de la mejor manera posible.'
+)
