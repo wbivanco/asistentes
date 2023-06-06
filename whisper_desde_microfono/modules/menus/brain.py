fc0a8b4cded5e2c25709f2523aaa54a6f0a60fa1
Walter Bivanco <walterbivanco@gmail.com>
2023-06-06 12:45:44 -0300

    Terminado el video con Notion

--- whisper_desde_microfono/modules/menus/brain.py
+++ whisper_desde_microfono/modules/menus/brain.py
@@ -21,13 +21,21 @@ def listen_to_response():
 
 def generate_response():
     response = langchain_assistant.chat(listen_to_response())
-    return response 
+    return eval(response.content)
 
 def create_notion_page(data):
-    pass
+    properties = {
+        "Name": {"title": [{"text": {"content": f"{data.get('title', None)}"}}]},        
+    } # OJO!, Name asi se llama el encabezado de la página en Notion y el segundo title es nombre 
+    # en el template de Langchain, sino devuelve None. 
+    children_page = [{"object": "block", "paragraph":{"rich_text": [{"text":{"content": f"{data.get('content', None)}"}}]}}] # Aquí
+    # es donde se carga en si el contenido de la página y que se accede desde el título cargado en el 
+    # paso anterior en Notion con el contenido del segundo content.
+    notion.create_page(properties=properties, children=children_page)
 
 def start_brain_mode():
     say_welcome()
     assistant_response = generate_response()
-    print(assistant_response.content)
-    talker.talk(assistant_response.content)
+    print(assistant_response.get('content', None))
+    talker.talk(assistant_response.get('content', None))
+    create_notion_page(assistant_response)
